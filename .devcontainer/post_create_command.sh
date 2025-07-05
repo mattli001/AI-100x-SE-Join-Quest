dcada8694ef5904a2a979fa1a213b388fab7fbf2
Matt Lee <mattli.tw@gmail.com>
2025-07-05 15:46:44 +0800

    chore: update devcontainer to support context7 & serena mcp

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -4,6 +4,8 @@ echo "post_create_command.sh -- start"
 echo "#### golang devloper tools #### "
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$(go env GOPATH)/bin" v1.60.3
 
+echo "#### install uv #### "
+curl -LsSf https://astral.sh/uv/install.sh | sh
 
 # add alias for user account 'vscode'
 {
---
+++ docs/insight.md
@@ -0,0 +1,8 @@
+# Insight
+
+參與水球軟體學院的「AI 軟工百倍研究組織」，有出入會任務作業，
+體驗的 exmaple 是 java，但不熟 java，小改一下 prompt，換用 golang 開發。用 cursor 的 Agent (claude-4-sonet) 跑一會後，真的有搞定，BDD 的測試通過...
+
+這種透過 BDD 的 Feature 文件先定義需求規格，再讓 AI 協助產生對應的程式碼，整個開發流程變得超級順暢。
+
+對於軟體開發變全自動化的可能性，有點興奮...
